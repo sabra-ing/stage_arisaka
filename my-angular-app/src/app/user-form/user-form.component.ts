6d91a9078be114bed4c9b5781b5128a3319b7f87
sabra-ing <benfadhelsabra592@gmail.com>
2025-08-06 01:22:02 +0100

    build the form

--- my-angular-app/src/app/user-form/user-form.component.ts
+++ my-angular-app/src/app/user-form/user-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from '../models/user.model';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-form',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent {
+   user: User = { name: '', email: '', age: 0 };
+  success: boolean = false;
+
+  constructor(private userService: UserService) {}
+
+  submitForm() {
+    this.userService.addUser(this.user).subscribe({
+      next: () => this.success = true,
+      error: (err) => console.error('Error:', err)
+    });
+  }
 
 }
