7b87b3fe2d0104365d7f5f473aa19d336668dbdc
sabra-ing <benfadhelsabra592@gmail.com>
2025-08-06 01:15:57 +0100

    Create the User Service

--- my-angular-app/src/app/user.service.ts
+++ my-angular-app/src/app/user.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { User } from './models/user.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor() { }
+    private apiUrl = 'https://backend_url/users';
+  constructor(private http: HttpClient) { }
+   addUser(user: User): Observable<User> { //observable  Handles async operations
+    return this.http.post<User>(this.apiUrl, user);
+  }
 }
